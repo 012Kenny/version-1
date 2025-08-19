286992c7a70b56f6c3a2f564814ecb59245c3a7a
012Kenny <136948157+012Kenny@users.noreply.github.com>
2025-08-19 15:08:21 +1200

    navigationscroll config
    trying to pass the threshold data so its easily configurable

--- NavigationScroll.js
+++ NavigationScroll.js
@@ -1,11 +1,17 @@
  
+const scriptTag = document.currentScript
+
+const thresholdData = parseFloat(scriptTag.getAttribute("data-threshold")) // this gets the threhold vairable so its configurable from the html
 
 
 
 // ------ Scroll nav ------ //
 window.addEventListener('scroll', function () {
 const navbar = document.getElementById('navbar');
-    if (window.scrollY > window.innerHeight * 0.2) { // goes below a certain height, + lower - higher
+    if (window.scrollY > window.innerHeight * thresholdData) {
+        return
+    }
+    if (window.scrollY > window.innerHeight * thresholdData) { // goes below a certain height, + lower - higher
     navbar.classList.add('scrolled');
     } else { // if scroll isn't higher than innerheight
     navbar.classList.remove('scrolled');
