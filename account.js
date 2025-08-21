154838603f47e55d0ff412794482b84ad703a5e8
012Kenny <136948157+012Kenny@users.noreply.github.com>
2025-08-21 20:30:19 +1200

    sign up/login working
    tested using the <a> text redirect, sign up seems not to be working though but login works

--- account.js
+++ account.js
@@ -1,4 +1,6 @@
+console.log("test")
 const express = require("express");
+console.log("test 2")
 const session = require("express-session");
 const bcrypt = require("bcryptjs");
 const sqlite3 = require("sqlite3").verbose();
@@ -6,6 +8,8 @@ const path = require("path");
 
 const app = express();
 const db = new sqlite3.Database("./users.db");
+const fs = require("fs");
+
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -21,17 +25,20 @@ app.use(
 
 app.use(express.static(path.join(__dirname)));
 
-db.run(
-
-);
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT
+  )
+`);
 
 
 app.get("/", (req, res) => {
     if (req.session.userId) {
-      res.send(`<h1>Welcome back, ${req.session.username}!</h1>
-                <a href="/logout">Logout</a>`);
-    } else {
-      res.redirect("/AccountCreateLogin/accountlogin.html");
+        res.sendFile(path.join(__dirname, "homepage", "../homepage/index.html"));
+        } else {
+      res.redirect("../AccountCreateLogin/accountlogin.html");
     }
   });
 
@@ -49,7 +56,7 @@ app.get("/", (req, res) => {
 
     stmt.run(username, hashedPassword, function (err) {
         if (err) {
-          return res.send("❌ Username already exists. <a href='/AccountCreateLogin/accountcreate.html'>Try again</a>");
+          return res.send("Username already exists. <a href=',,/AccountCreateLogin/accountcreate.html'>Try again</a>");
         }
 
     req.session.userId = this.lastID;
@@ -57,5 +64,27 @@ app.get("/", (req, res) => {
     res.redirect("/");
   });
 });
+
+app.post("/login", (req, res) => {
+    const {username, password} = req.body;
+    db.get("SELECT * FROM users WHERE username = ?", [username], (err, user) => {
+        if (!user) return res.send(" Invalid username or password. <a href='../AccountCreateLogin/accountlogin.html'>Try again</a>");        
+        const validPass = bcrypt.compareSync(password, user.password);
+        if (!validPass) return res.send("Invalid username or password. <a href='../AccountCreateLogin/accountlogin.html'>Try again</a>");
+    
+    req.session.userId = user.id;
+    req.session.username = user.username;
+    res.redirect("/");
+    });
+});
+
   
 
+app.get("/logout", (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/AccountCreateLogin/accountlogin.html");
+    });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
