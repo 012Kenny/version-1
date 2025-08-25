f8813f063d260f60768c67f0a51eac486fa456a1
012Kenny <136948157+012Kenny@users.noreply.github.com>
2025-08-25 13:47:31 +1200

    client chat 1

--- Community/chat.js
+++ Community/chat.js
@@ -2,5 +2,42 @@ const socket = io();
 
 
 //** ------- variables ------- **//
-const joinBtn = document.querySelector(".room");
-const chatroomJoin =
+const joinBtn = document.querySelector(".join-btn");
+const chatroomJoinSection = document.querySelector(".chatroom-join");
+const actualChatroom = document.querySelector(".actual-chatroom");
+
+const leaveChatBtn = document.getElementById("leave-chat");
+const chatMessages = document.getElementById("chat-messages");
+const chatForm = document.getElementById('chat-form');
+const chatInput = document.getElementById("chat-input");
+
+let username = document.getElementById("account-username").textContent;
+
+actualChatroom.style.display = "none";
+
+
+
+//** ------- join chat ------- **//
+
+joinBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    socket.emit("join", username);
+    chatroomJoinSection.style.display = 'none';
+    actualChatroom.style.display = 'block';
+});
+
+
+
+//** ------- leave chat ------- **//
+leaveChatBtn.addEventListener("click", () => {
+    socket.disconnect();
+    socket.connect();
+
+    chatroomJoinSection.style.display - 'block';
+    actualChatroom.style.display = 'none';
+});
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const msg = chatInput.ariaValueMax.trim();
+})
